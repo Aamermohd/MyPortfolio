08b5d7af310add014a0b0faa3e6716d78e1ad100
Mohammed Amir Khan <mohkhan@innominds.com>
2022-08-03 11:53:20 +0530

    second commit

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "FullStackWebDeveloper" , "Freelancer"],
+        strings: ["Backened Developer", "FullStackWebDeveloper" , "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "Developer", "FullStackWebDeveloper" , "Freelancer"],
+        strings: [ "Backened Developer", "FullStackWebDeveloper" , "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
